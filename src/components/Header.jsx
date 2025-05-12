424928386149ec29efce5b759fbe449f95786d05
yunus kupucu <yunus.kupucu@gmail.com>
2025-05-12 20:30:49 +0300

    refactor layout and add header component with theme toggle

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import logo from '../images/logo.png';
+import '../css/Header.css';
+import { CiShoppingBasket } from 'react-icons/ci';
+import { CiLight } from 'react-icons/ci';
+import { FaMoon } from 'react-icons/fa';
+
+function Header() {
+  const [theme, setTheme] = useState(false);
+
+  const changeTheme = () => {
+    const root = document.getElementById('root');
+    if (theme) {
+      root.style.backgroundColor = '#1D2A35';
+      root.style.color = '#fff';
+    } else {
+      root.style.backgroundColor = 'white';
+      root.style.color = 'black';
+    }
+    setTheme(!theme);
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <div className="flex-row">
+        <img className="logo" src={logo} />
+        <p className="brand">E-COMMERCE</p>
+      </div>
+      <div className="flex-row">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Arama yapabilirsiniz"
+        />
+        <div>
+          {theme ? (
+            <FaMoon className="icon" onClick={changeTheme} />
+          ) : (
+            <CiLight className="icon" onClick={changeTheme} />
+          )}
+          <CiShoppingBasket className="icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;

b799056e11217ab5d439c950a0908a990423982b
yunus kupucu <yunus.kupucu@gmail.com>
2025-05-15 23:17:07 +0300

    add ProductDetails component and configure routing for product detail view

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import '../css/Product.css';
+import { useNavigate } from 'react-router-dom';
 
 function Product({ product }) {
   const { id, title, price, image, description } = product;
+  const navigate = useNavigate();
   console.log(product);
 
   return (
     <div className="card">
       <img className="image" src={image} alt={title} />
       <div>
-        <p style={{ textAlign: 'center' }}>{title}</p>
+        <p style={{ textAlign: 'center', height: '60px' }}>{title}</p>
         <h3 style={{ textAlign: 'center' }}>{price} $</h3>
       </div>
       <div className="button-div">
-        <button className="detail-button">Detaya Git</button>
+        <button
+          onClick={() => navigate('/product-detail/' + id)}
+          className="detail-button"
+        >
+          Detaya Git
+        </button>
       </div>
     </div>
   );
